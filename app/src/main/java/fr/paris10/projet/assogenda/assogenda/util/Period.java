51e86a21eb4c7390f29edcf33478b4b68b09fcdf
Valentin Bouquet <valentin-bouquet@hotmail.fr>
2017-01-22 18:16:36 +0100

    Fix constructor to identify wrong period

--- app/src/main/java/fr/paris10/projet/assogenda/assogenda/util/Period.java
+++ app/src/main/java/fr/paris10/projet/assogenda/assogenda/util/Period.java
@@ -8,11 +8,13 @@ public class Period {
     private int begin;
     private int end;
 
-    public Period(int begin, int end) throws PeriodException{
-        if(begin > end || begin == end)
-            throw new PeriodException("Begin must be lower or different than end");
-        if(begin < 0 || begin > 24 || end < 0 || begin > 24 || begin > end)
-            throw new PeriodException("A period can't exceed 24hours or have negative values");
+    public Period(int begin, int end) throws PeriodException {
+        if (begin > end || begin == end)
+            throw new PeriodException("Begin must be inferior to the end of the period");
+        if (begin < 0 || begin > 24)
+            throw new PeriodException("Begin of period can't be negative of exceed 24 hours");
+        if (end < 0 || end > 24)
+            throw new PeriodException("End of a period can't be negative or exceed 24 hours");
         this.begin = begin;
         this.end = end;
     }
