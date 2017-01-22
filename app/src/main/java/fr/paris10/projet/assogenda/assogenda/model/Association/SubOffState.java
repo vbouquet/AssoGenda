e87696d0480aad88aa42e0af56cfb1538ee68577
Valentin Bouquet <valentin-bouquet@hotmail.fr>
2017-01-22 17:18:23 +0100

    Add default models for association

---
+++ app/src/main/java/fr/paris10/projet/assogenda/assogenda/model/Association/SubOffState.java
@@ -0,0 +1,9 @@
+package fr.paris10.projet.assogenda.assogenda.model.Association;
+
+import fr.paris10.projet.assogenda.assogenda.model.Association.AssociationState;
+
+/**
+ * Association's subscription is over, she is not allowed to create new events.
+ */
+class SubOffState implements AssociationState {
+}
---
+++ app/src/main/java/fr/paris10/projet/assogenda/assogenda/model/Association/SubOnState.java
@@ -0,0 +1,7 @@
+package fr.paris10.projet.assogenda.assogenda.model.Association;
+
+/**
+ * Association's subscription is still on, she can create events.
+ */
+class SubOnState implements AssociationState {
+}
