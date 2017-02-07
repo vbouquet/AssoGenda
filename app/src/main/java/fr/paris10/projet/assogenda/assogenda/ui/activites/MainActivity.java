679706af4d810098292612092570e8f47e9d2d4b
wilpiron <william.piron@laposte.net>
2017-02-07 13:25:07 +0100

    Branch creation, activities creation, editing.

--- app/src/main/java/fr/paris10/projet/assogenda/assogenda/ui/activites/MainActivity.java
+++ app/src/main/java/fr/paris10/projet/assogenda/assogenda/ui/activites/MainActivity.java
@@ -1,15 +1,126 @@
 package fr.paris10.projet.assogenda.assogenda.ui.activites;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import fr.paris10.projet.assogenda.assogenda.R;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Initialize Firebase Auth and Database Reference
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        if (mFirebaseUser == null) {
+            // Not logged in, launch the Log In activity
+            loadLogInView();
+        } else {
+            mUserId = mFirebaseUser.getUid();
+
+            // Set up ListView
+            final ListView listView = (ListView) findViewById(R.id.listView);
+            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+            listView.setAdapter(adapter);
+
+            // Add items via the Button and EditText at the bottom of the view.
+            final EditText text = (EditText) findViewById(R.id.todoText);
+            final Button button = (Button) findViewById(R.id.addButton);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    mDatabase.child("users").child(mUserId).child("items").push().child("title").setValue(text.getText().toString());
+                    text.setText("");
+                }
+            });
+
+            // Use Firebase to populate the list.
+            mDatabase.child("users").child(mUserId).child("items").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    adapter.add((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    adapter.remove((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            // Delete items when clicked
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    mDatabase.child("users").child(mUserId).child("items")
+                            .orderByChild("title")
+                            .equalTo((String) listView.getItemAtPosition(position))
+                            .addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if (dataSnapshot.hasChildren()) {
+                                        DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                                        firstChild.getRef().removeValue();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
