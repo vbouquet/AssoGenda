9eddc8676a329a4182629fed6ae25511d50301ec
remypoc <remypoc@free.fr>
2017-02-28 12:33:33 +0100

    refactor (Association) : refactor test association

--- app/src/androidTest/java/fr/paris10/projet/assogenda/assogenda/ui/fragment/AssociationMainFragmentTest.java
+++ app/src/androidTest/java/fr/paris10/projet/assogenda/assogenda/ui/fragment/AssociationMainFragmentTest.java
@@ -1,14 +1,8 @@
 package fr.paris10.projet.assogenda.assogenda.ui.fragment;
 
-import android.support.test.espresso.intent.rule.IntentsTestRule;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 import fr.paris10.projet.assogenda.assogenda.R;
-import fr.paris10.projet.assogenda.assogenda.ui.activites.AssociationDashboardActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -16,22 +10,8 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.*;
 
 public class AssociationMainFragmentTest {
-
-    @Rule
-    public IntentsTestRule<AssociationDashboardActivity> mActivityRule = new IntentsTestRule<>(AssociationDashboardActivity.class);
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
 
     @Test
     public void testCreateAssociationButton() {
