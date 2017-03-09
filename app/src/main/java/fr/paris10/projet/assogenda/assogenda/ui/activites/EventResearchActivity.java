16d88e7411b0452b776f98acf382181f7d268f40
repocque <remypoc@free.fr>
2017-03-09 10:01:26 +0100

    feat(EventResearch) : add activity for event research

---
+++ app/src/main/java/fr/paris10/projet/assogenda/assogenda/ui/activites/EventResearchActivity.java
@@ -0,0 +1,15 @@
+package fr.paris10.projet.assogenda.assogenda.ui.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.paris10.projet.assogenda.assogenda.R;
+
+public class EventResearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_research);
+    }
+}
