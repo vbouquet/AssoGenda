1a481894a256516c225a404cda5b65be6279b6c6
Valentin Bouquet <valentin-bouquet@hotmail.fr>
2017-03-24 20:02:41 +0100

    Fixed association logo in view

--- app/src/main/java/fr/paris10/projet/assogenda/assogenda/ui/activites/ShowAssociationActivity.java
+++ app/src/main/java/fr/paris10/projet/assogenda/assogenda/ui/activites/ShowAssociationActivity.java
@@ -4,17 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 import fr.paris10.projet.assogenda.assogenda.R;
@@ -22,25 +27,23 @@ import fr.paris10.projet.assogenda.assogenda.model.Association;
 import fr.paris10.projet.assogenda.assogenda.model.User;
 
 public class ShowAssociationActivity extends AppCompatActivity {
-    private String associationID;
+    private String      associationID;
     private Association association;
-    private User president;
-    private TextView nameAsso;
-    private TextView descAsso;
-    private Button createEvent;
-    private ImageView image;
+    private TextView    nameAsso;
+    private TextView    descAsso;
+    private Button      createEvent;
+    private ImageView   logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_association);
 
-        image = (ImageView) findViewById(R.id.activity_show_association_logo_asso);
-        nameAsso = (TextView) findViewById(R.id.activity_show_association_name_asso);
-        descAsso = (TextView) findViewById(R.id.activity_show_association_description_asso);
-        createEvent = (Button) findViewById(R.id.activity_show_association_create_event);
-        associationID = (String) getIntent().getExtras().get("associationID");
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("association");
+        logo           = (ImageView) findViewById(R.id.activity_show_association_logo_asso);
+        nameAsso        = (TextView) findViewById(R.id.activity_show_association_name_asso);
+        descAsso        = (TextView) findViewById(R.id.activity_show_association_description_asso);
+        createEvent     = (Button) findViewById(R.id.activity_show_association_create_event);
+        associationID   = (String) getIntent().getExtras().get("associationID");
 
         nameAsso.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,33 +53,33 @@ public class ShowAssociationActivity extends AppCompatActivity {
         });
 
         descAsso.setMovementMethod(new ScrollingMovementMethod());
-        reference.child(associationID).addListenerForSingleValueEvent(new ValueEventListener() {
+        FirebaseDatabase.getInstance()
+                .getReference("association")
+                .child(associationID)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     association = dataSnapshot.getValue(Association.class);
                     nameAsso.setText(association.name);
                     descAsso.setText(association.description);
-                    DatabaseReference references = FirebaseDatabase.getInstance().getReference("users");
-                    references.child(association.president).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            if (dataSnapshot.exists()) {
-                                president = dataSnapshot.getValue(User.class);
-                            }
-                        }
 
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
+                    if (association.logo != null) {
+                        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
 
-                        }
-                    });
+                        StorageReference imagePath = storageReference.child(association.logo);
+
+                        Glide.with(getApplicationContext())
+                                .using(new FirebaseImageLoader())
+                                .load(imagePath)
+                                .into(logo);
+                    }
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.d("onCancelled", databaseError.getMessage());
             }
         });
 
